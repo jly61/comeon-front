23328f49077aab5d981e41760393391d38b54ea5
jly <896503709@qq.com>
2022-07-07 21:29:06 +0800

    前后端联调测试

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://47.105.106.78',
+        changeOrigin: true, // 虚拟的站点需要更管origin
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 })
